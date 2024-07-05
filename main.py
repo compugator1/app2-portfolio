324438078ee3dea0ded9f6cfe72097b310012c11
compugator1 <mamaglio@yahoo.com>
2024-07-05 16:46:45 -0500

    Dat 21: Added description on the new columns which explains what is below

--- main.py
+++ main.py
@@ -9,8 +9,15 @@ with col1:
 
 with col2:
     st.title("Mark Maglio")
-    content = """
+    info_content = """
     Hello, I am Mark Maglio.  I am an electrical engineer who has programmed for over 40 years.\n\n
     I am looking forward to showing you what I have built with Python since I started working with it.
     """
-    st.info(content)
+    st.info(info_content)
+
+# better to use this method of inputting content to be pronted rather than editing the python commands directly
+content = """
+Below you can find some of the apps I have built in Python.  Feel free to contact me!
+"""
+st.write(content)
+
